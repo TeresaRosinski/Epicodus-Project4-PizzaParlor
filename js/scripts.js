8b98c798cdc3a2fd1a6cf7eba9c115e99e3bc5d9
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-19 11:11:50 -0500

    Add TotalOrder Constructor function, addCreatedPizza prototype, CreatedPizza Constructor function, addToppings prototype

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//Business Logic
+
+//Constructor function to hold to complete order a user creates
+function TotalOrder (){
+  this.createdPizzas=[]
+  this.orderId=0 // might not be needed - could be good if someone orders more than one things to track the price of each thing ordered
+  this.price=[]///put in price function here?
+}
+
+TotalOrder.prototype.addCreatedPizza = function (createdPizza){
+  createdPizza.id = this.assignId();
+  this.createdPizzas.push(createdPizza);
+}
+
+//Constructor function to hold each pizza a user creates
+function CreatedPizza (size) {
+  this.toppings=[];  //length of array can be measured to determine price if each topping cost $1.50
+  this.size=size; 
+}
+
+CreatedPizza.prototype.AddToppings = function (toppings){
+  this.toppings.push(toppings);
+}
+
+let pizza1 = new CreatedPizza ( "small" )
+pizza1.AddToppings("Basil Pepperoni Olives");
+console.log(pizza1);
+//User Interface Logic
