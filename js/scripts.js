35be3249150708b9be664fe450e1cef8ca70be01
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-19 12:19:01 -0500

    Add CreatedPizza price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -18,21 +18,53 @@ TotalOrder.prototype.assignId= function () {
 }
 
 //Constructor function to hold each pizza a user creates
-function CreatedPizza (size) {
+function CreatedPizza(size){
   this.toppings=[];  //length of array can be measured to determine price if each topping cost $1.50
   this.size=size; 
+  this.price=[];
 }
 
-CreatedPizza.prototype.AddToppings = function (toppings){
+CreatedPizza.prototype.AddToppings=function(toppings){
   this.toppings.push(toppings);
 }
-
  
+CreatedPizza.prototype.PriceCalculationPerPizza=function() {
+  let Price=7;
+  debugger; 
+  if(this.toppings.length >=1){
+    for (let i= 0; i<this.toppings.length; i++){
+      Price+=1;
+    }
+  }if (this.size === "small"){
+    Price+=0;
+  }else if(this.size === "medium"){
+    Price +=2;
+  }else if(this.size === "large"){
+    Price +=4; 
+  }
+  console.log(Price);
+  return Price;
+}
+
+CreatedPizza.prototype.PushPricePerPizza=function(Price){
+  this.price.push(Price);
+}
+
 let pizza1 = new CreatedPizza ( "small" )
-pizza1.AddToppings("Basil Pepperoni Olives");
+pizza1.AddToppings("Basil");
+pizza1.AddToppings("f");
+pizza1.AddToppings("d");
+let pricePizza1=pizza1.PriceCalculationPerPizza();
+pizza1.PushPricePerPizza(pricePizza1);
+ 
+let pizza2 = new CreatedPizza ("medium")
+pizza2.AddToppings("Cheese")
+
+pizza1.PriceCalculationPerPizza(pizza1);
 
 let order1 = new TotalOrder ();
 order1.AddCreatedPizza(pizza1);
+order1.AddCreatedPizza(pizza2);
 console.log(pizza1);
 console.log(order1);
 //User Interface Logic
